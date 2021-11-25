0c215ca1801423942683fa82927592e36ee1d573
Kerry <trankerry94@gmail.com>
2021-11-24 16:10:17 -0800

    finished up to #4

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+
+  componentWillMount() {
+    // Here we're simply invoking the axios object, specifically..
+    Axios
+      // .. the .get function. Here we pass the URL of our external service.
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      // Here the first fulfill callback is setup.
+      // This callback simply takes an HTTP response and returns the data property..
+      .then(response => response.data)
+      // .. to the second fulfill callback, which uses React's this.setState
+      // function to merge the provided object (which uses ES6 shorthand)
+      // with the current object assigned to this.state (which you assigned
+      // in your constructor!)
+      .then(topspots => this.setState({ topspots }));
+  }
+
   render() {
     return (
-      <div className='App'></div>
+      <div className='App container'>
+        <div className='jumbotron'>
+          <h1>San Diego Top Spots</h1>
+          <p>A list of the top 30 places to see in San Diego, California</p>
+        </div>
+        <pre>{ JSON.stringify(this.state.topspots, null, 2) }</pre>
+      </div>
     );
   }
 }
 
-export default App;
